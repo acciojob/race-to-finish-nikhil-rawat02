4e887be3712bce8824baa696d5f309f9935de45e
nikhil-rawat02 <117645790+nikhil-rawat02@users.noreply.github.com>
2023-04-23 15:18:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,46 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+let output = document.getElementById("output");
+let p1 = new Promise((resolve,reject) =>{
+	setInterval(()=>{
+		resolve("promise1 resolve");
+		
+	},1000)
+})
+
+let p2 = new Promise((resolve,reject) =>{
+	setInterval(()=>{
+		resolve("promise2 resolve");
+		
+	},2000)
+})
+
+let p3 = new Promise((resolve,reject) =>{
+	setInterval(()=>{
+		resolve("promise3 resolve");
+		
+	},3000)
+})
+
+let p4 = new Promise((resolve,reject) =>{
+	setInterval(()=>{
+		resolve("promise4 resolve");
+		
+	},4000)
+})
+
+let p5 = new Promise((resolve,reject) =>{
+	setInterval(()=>{
+		resolve("promise5 resolve");
+		
+	},5000)
+})
+promises.push(p2);
+promises.push(p4);
+promises.push(p5);
+promises.push(p3);
+promises.push(p1);
+Promise.any(promises).then((value) => {
+	output.innerHTML = value;
+})
